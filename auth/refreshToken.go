26492ec7c7d952d99e2ba3668c0785ebb5fc521b
bcetienne <etienne1296pro@gmail.com>
2025-07-10 08:13:37 +0200

    Refresh token

--- auth/refreshToken.go
+++ auth/refreshToken.go
@@ -9,6 +9,9 @@ type RefreshTokenService struct {
 type RefreshTokenServiceInterface interface {
 	CreateRefreshToken(userID int) (string, error)
 	VerifyRefreshToken(token string) (int, error)
+	RevokeRefreshToken(token string, userID int) error
+	RevokeAllUserRefreshTokens(userID int) error
+	FlushRefreshTokens() error
 }
 
 func NewRefreshTokenService(config *lib.Config) RefreshTokenService {
@@ -21,4 +24,13 @@ func (rts *RefreshTokenService) CreateRefreshToken(userID int) (string, error) {
 }
 
 func (rts *RefreshTokenService) VerifyRefreshToken(token string) (int, error) {
+}
+
+func (rts *RefreshTokenService) RevokeRefreshToken(token string, userID int) error {
+}
+
+func (rts *RefreshTokenService) RevokeAllUserRefreshTokens(userID int) error {
+}
+
+func (rts *RefreshTokenService) FlushRefreshTokens() error {
 }
