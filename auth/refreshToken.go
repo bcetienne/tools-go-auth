c03e7e0a84f21238d6fef016b696e43b308b86a6
bcetienne <etienne1296pro@gmail.com>
2025-07-12 15:27:40 +0200

    On constructor, create the table if not exists

--- auth/refreshToken.go
+++ auth/refreshToken.go
@@ -1,8 +1,13 @@
 package auth
 
-import "gitlab.com/bcstudio1/tools/go-auth/lib"
+import (
+	"database/sql"
+
+	"gitlab.com/bcstudio1/tools/go-auth/lib"
+)
 
 type RefreshTokenService struct {
+	db     *sql.DB
 	config *lib.Config
 }
 
@@ -14,10 +19,68 @@ type RefreshTokenServiceInterface interface {
 	FlushRefreshTokens() error
 }
 
-func NewRefreshTokenService(config *lib.Config) RefreshTokenService {
-	return RefreshTokenService{
+func NewRefreshTokenService(db *sql.DB, config *lib.Config) (*RefreshTokenService, error) {
+	service := &RefreshTokenService{
+		db:     db,
 		config: config,
 	}
+
+	var exists bool
+
+	// Does schema exists ?
+	queryExists := `
+	SELECT EXISTS (
+		SELECT FROM information_schema.tables
+		WHERE table_schema = 'go_auth'
+	)
+	`
+	err := db.QueryRow(queryExists).Scan(&exists)
+	if err != nil {
+		return nil, err
+	}
+	if !exists {
+		// Create the schema
+		_, err = db.Exec(`CREATE SCHEMA go_auth;`)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Does table exists ?
+	queryExists = `
+	SELECT EXISTS (
+		SELECT FROM information_schema.tables
+		WHERE table_schema = 'go_auth'
+		AND table_name = 'refresh_token'
+	)
+	`
+	err = db.QueryRow(queryExists).Scan(&exists)
+	if err != nil {
+		return nil, err
+	}
+	if !exists {
+		// Create the table
+		query := `
+		CREATE TABLE refresh_token (
+			refresh_token_id SERIAL PRIMARY KEY,
+			user_id INT NOT NULL,
+			token VARCHAR NOT NULL,
+			expires_at TIMESTAMPTZ NOT NULL,
+			created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			revoked_at TIMESTAMPTZ,
+			UNIQUE(token)
+		);
+		COMMENT ON TABLE refresh_token IS 'Refresh tokens for user authentication';
+		CREATE INDEX idx_refresh_token_token ON refresh_token(token);
+		CREATE INDEX idx_refresh_token_expires_at ON refresh_token(expires_at);
+		`
+		_, err = db.Exec(query)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return service, nil
 }
 
 func (rts *RefreshTokenService) CreateRefreshToken(userID int) (string, error) {
