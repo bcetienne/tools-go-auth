5ccd00395812cb142a4b49bb75f6489db5ac117d
bcetienne <etienne1296pro@gmail.com>
2025-07-14 21:26:37 +0200

    Add tests for VerifyAccessToken with various scenarios

--- tests/auth/accessToken_test.go
+++ tests/auth/accessToken_test.go
@@ -1,11 +1,14 @@
 package auth
 
 import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"gitlab.com/bcstudio1/tools/go-auth/auth"
 	"gitlab.com/bcstudio1/tools/go-auth/lib"
 	"gitlab.com/bcstudio1/tools/go-auth/model"
+	"log"
 	"testing"
-
-	"gitlab.com/bcstudio1/tools/go-auth/auth"
+	"time"
 )
 
 func Test_Auth_AccessToken_CreateAccessToken_TableDriven(t *testing.T) {
@@ -57,4 +60,137 @@ func Test_Auth_AccessToken_CreateAccessToken_TableDriven(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_Auth_AccessToken_VerifyAccessToken_TableDrive(t *testing.T) {
+	tests := []struct {
+		testName      string
+		token         string
+		expectSuccess bool
+	}{
+		{
+			testName:      "Success",
+			token:         "",
+			expectSuccess: true,
+		},
+		{
+			testName:      "Fail - Bad token",
+			token:         "bad_token",
+			expectSuccess: false,
+		},
+	}
+
+	user := model.AuthUser{
+		UserID:   2,
+		UserUUID: "456-456-456",
+		Email:    "miss@mail.com",
+	}
+	config := lib.Config{
+		Issuer:             "test_auth.com",
+		JWTSecret:          "secureStr1ng_",
+		JWTExpiry:          "12h",
+		RefreshTokenExpiry: "3h",
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			accessTokenService := auth.NewAccessTokenService(&config)
+			token, err := accessTokenService.CreateAccessToken(&user)
+			if err != nil {
+				t.Fatalf("The test expect no error on access token creation, got : %v", err)
+			}
+			if len(tt.token) == 0 {
+				tt.token = token
+			}
+
+			verified, err := accessTokenService.VerifyAccessToken(token)
+
+			if tt.expectSuccess {
+				if err != nil {
+					t.Fatalf("The test expect no error, got : %v", err)
+				}
+				if verified.UserID != user.UserID {
+					t.Fatalf("Ther user ID does not match. Expected %d, got %d", user.UserID, verified.UserID)
+				}
+				if verified.Issuer != config.Issuer {
+					if verified.Subject != user.Email {
+						t.Fatalf("The subject does not match. Expected %s, got %s", user.Email, verified.Subject)
+					}
+					t.Fatalf("The issuer does not match. Expected %s, got %s", config.Issuer, verified.Issuer)
+				}
+			}
+		})
+	}
+}
+
+func Test_Auth_AccessToken_VerifyAccessToken_Expired(t *testing.T) {
+	// This should be a "Success" either with an "error" because, the claim should not be NIL.
+	// But it should be with a specific error when the token is expired.
+	t.Run("Success - Expired token", func(t *testing.T) {
+		user := model.AuthUser{
+			UserID:   3,
+			UserUUID: "789-789-789",
+			Email:    "mister@mail.com",
+		}
+		config := lib.Config{
+			Issuer:             "test_auth.com",
+			JWTSecret:          "passw0rdHidden_",
+			JWTExpiry:          "1s",
+			RefreshTokenExpiry: "6h",
+		}
+		accessTokenService := auth.NewAccessTokenService(&config)
+		token, err := accessTokenService.CreateAccessToken(&user)
+		if err != nil {
+			t.Fatalf("The test expect no error on access token creation, got : %v", err)
+		}
+
+		log.Printf("Sleeping test for 5 seconds...")
+		time.Sleep(5 * time.Second)
+
+		verified, err := accessTokenService.VerifyAccessToken(token)
+		if err == nil {
+			t.Fatal("The error should not be nil")
+		}
+		if !errors.Is(err, jwt.ErrTokenExpired) {
+			t.Fatal("The error type should be a JWT Token Expired error")
+		}
+		if verified == nil {
+			t.Fatal("The claim should not be NIL")
+		}
+	})
+}
+
+func Test_Auth_AccessToken_VerifyAccessToken_TwoDifferentTokens(t *testing.T) {
+	t.Run("Success - Two different tokens", func(t *testing.T) {
+		user := model.AuthUser{
+			UserID:   3,
+			UserUUID: "789-789-789",
+			Email:    "mister@mail.com",
+		}
+		secondUser := model.AuthUser{
+			UserID:   4,
+			UserUUID: "000-000-000",
+			Email:    "lady@mail.com",
+		}
+		config := lib.Config{
+			Issuer:             "test_auth.com",
+			JWTSecret:          "super_Str0ngStr1ng_",
+			JWTExpiry:          "4h",
+			RefreshTokenExpiry: "2h",
+		}
+		accessTokenService := auth.NewAccessTokenService(&config)
+		token, err := accessTokenService.CreateAccessToken(&user)
+		if err != nil {
+			t.Fatalf("The test expect no error on access token creation, got : %v", err)
+		}
+
+		secondToken, err := accessTokenService.CreateAccessToken(&secondUser)
+		if err != nil {
+			t.Fatalf("The test expect no error on access token creation, got : %v", err)
+		}
+
+		if token == secondToken {
+			t.Fatalf("Two users should not have the same token !")
+		}
+	})
 }
